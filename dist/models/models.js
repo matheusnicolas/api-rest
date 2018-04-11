aaf01c7fcb42881c246c3e5ff47dbcd79271471e
oguiraw <sergio.ewerton@dce.ufpb.br>
2018-04-11 17:03:26 -0300

    alterações nos models. Todas as relações feitas

--- dist/models/models.js
+++ dist/models/models.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.Nota = exports.Disciplina = exports.Sala = exports.Turma = exports.User = undefined;
+exports.AlunoProfTurma = exports.ProfTurma = exports.ProfDisc = exports.Frequencia = exports.Nota = exports.Disciplina = exports.Sala = exports.Turma = exports.User = undefined;
 
 var _sequelize = require('sequelize');
 
@@ -17,14 +17,21 @@ var sequelize = new _sequelize2.default('kula_bd', null, null, {
     storage: './data.sqlite'
 });
 
+// let sequelize = new Sequelize('kula_bd', 'kula', 'kula123456', {
+//     host: 'localhost',
+//     port: '5432',
+//     dialect: 'postgres',
+//     operatorsAliases: false
+// });
+
 var User = exports.User = sequelize.define('user', {
+    id: { type: _sequelize2.default.INTEGER, primaryKey: true, autoIncrement: true
+    },
     nome: _sequelize2.default.STRING,
     sobrenome: _sequelize2.default.STRING,
     cpf: { type: _sequelize2.default.STRING, unique: true },
     username: { type: _sequelize2.default.STRING, allowNull: false, unique: true },
     password: { type: _sequelize2.default.STRING, allowNull: false },
-    id: { type: _sequelize2.default.INTEGER, primaryKey: true, autoIncrement: true
-    },
     matricula: { type: _sequelize2.default.STRING, allowNull: false, unique: true },
     sexo: _sequelize2.default.STRING,
     email: { type: _sequelize2.default.STRING, allowNull: false, unique: true },
@@ -32,15 +39,12 @@ var User = exports.User = sequelize.define('user', {
 });
 
 var Turma = exports.Turma = sequelize.define('turma', {
-    sigla: _sequelize2.default.STRING,
-    serie: _sequelize2.default.INTEGER,
-    sala: _sequelize2.default.STRING,
-    aluno: _sequelize2.default.STRING,
-    professor: _sequelize2.default.STRING,
     id: {
         type: _sequelize2.default.INTEGER, autoIncrement: true, primaryKey: true
-    }
-
+    },
+    sigla: _sequelize2.default.STRING,
+    serie: _sequelize2.default.INTEGER,
+    sala: _sequelize2.default.STRING
 });
 
 var Sala = exports.Sala = sequelize.define('sala', {
@@ -64,15 +68,45 @@ var Disciplina = exports.Disciplina = sequelize.define('disciplina', {
 });
 
 var Nota = exports.Nota = sequelize.define('nota', {
-    aluno: _sequelize2.default.STRING,
+    id: {
+        type: _sequelize2.default.INTEGER, autoIncrement: true, primaryKey: true
+    },
     nota: _sequelize2.default.DOUBLE,
     unidade: _sequelize2.default.INTEGER,
     bimestre: _sequelize2.default.INTEGER,
-    professor: _sequelize2.default.STRING,
-    id: {
-        type: _sequelize2.default.INTEGER, autoIncrement: true, primaryKey: true
-    }
+    alunoProfTurmaId: { type: _sequelize2.default.INTEGER, allowNull: false }
 });
+
+var Frequencia = exports.Frequencia = sequelize.define('frequencia', {
+    data: _sequelize2.default.DATE,
+    presenca: _sequelize2.default.BOOLEAN,
+    alunoProfTurmaId: { type: _sequelize2.default.INTEGER, allowNull: false }
+});
+
+var ProfDisc = exports.ProfDisc = sequelize.define('profDisc', {
+    id: { type: _sequelize2.default.INTEGER, primaryKey: true, autoIncrement: true }
+});
+
+var ProfTurma = exports.ProfTurma = sequelize.define('profTurma', {
+    id: { type: _sequelize2.default.INTEGER, primaryKey: true, autoIncrement: true }
+});
+
+var AlunoProfTurma = exports.AlunoProfTurma = sequelize.define('alunoProfTurma', {
+    id: { type: _sequelize2.default.INTEGER, primaryKey: true, autoIncrement: true }
+});
+
+User.belongsToMany(Disciplina, { through: 'profDisc' });
+Disciplina.belongsToMany(User, { through: 'profDisc' });
+
+User.belongsToMany(Turma, { through: 'profTurma' });
+Turma.belongsToMany(User, { through: 'profTurma' });
+
+User.belongsToMany(ProfTurma, { through: 'alunoProfTurma' });
+ProfTurma.belongsToMany(User, { through: 'alunoProfTurma' });
+
+Nota.belongsTo(AlunoProfTurma, { foreignKey: 'alunoProfTurmaId' });
+
+Frequencia.belongsTo(AlunoProfTurma, { through: 'alunoProfTurmaId' });
 
 //Turma.hasMany(User, {foreignKey: 'aluno', sourceKey: 'id'});
 //User.belongsTo(Turma, {foreignKey: 'aluno', targetKey: 'id'});
@@ -82,3 +116,7 @@ Turma.sync();
 Sala.sync();
 Disciplina.sync();
 Nota.sync();
+ProfDisc.sync();
+ProfTurma.sync();
+AlunoProfTurma.sync();
+Frequencia.sync();
